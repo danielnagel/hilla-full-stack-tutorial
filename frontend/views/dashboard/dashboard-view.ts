694437e67b70a7f24ec03eb28f6de65fda62983a
Daniel Nagel <danielnagel@mailbox.org>
2023-01-29 19:40:54 +0100

    Added dashboard view

--- frontend/views/dashboard/dashboard-view.ts
+++ frontend/views/dashboard/dashboard-view.ts
@@ -1,6 +1,9 @@
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { View } from 'Frontend/views/view';
+import '@vaadin/charts';
+import '@vaadin/charts/src/vaadin-chart-series';
+import { dashboardViewStore } from './dashboard-view-store';
 
 @customElement('dashboard-view')
 export class DashboardView extends View {
@@ -10,6 +13,26 @@ export class DashboardView extends View {
   }
 
   render() {
-    return html`<h1>Dashboard view</h1>`;
+    return html`
+      <div class="text-xl mb-xl">
+        ${dashboardViewStore.contactCount} contacts
+      </div>
+  
+      ${this.getCompanyStats()}
+    `;
+  }
+
+  getCompanyStats() {
+    if (dashboardViewStore.companyStats.length === 0) {
+      return html`<p>Loading stats...</p>`;
+    } else {
+      return html`
+        <vaadin-chart type="pie">
+          <vaadin-chart-series
+            .values=${dashboardViewStore.companyStats}
+          ></vaadin-chart-series>
+        </vaadin-chart>
+      `;
+    }
   }
 }
